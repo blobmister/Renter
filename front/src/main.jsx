74b9a045211cda78cdd926be88fe4d17d6a6b858
Zhitian Mai <zmai0594@uni.sydney.edu.au>
2025-08-30 14:45:01 +1000

    setup react router to home and login page

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import Login from "./Login.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
