a60655b830083e1b4e3a772983c57900211228a6
Krish Gupta <stockkrishgupta@gmail.com>
2025-08-30 20:16:06 +1000

    All user apis have been made

--- back/src/controllers/user.controller.js
+++ back/src/controllers/user.controller.js
@@ -65,13 +65,76 @@ const createReview = async (req, res) => {
                 stars: stars,
             }])
         if (error) {
+            return res.status(400).json({error: error.message});
+        }
+
+        return res.status(200).json({data: data[0]});
+    } catch (err) {
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+}
+
+const getUserInfo = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+
+        const { data, error } = await supabase
+            .from('auth.users')
+            .select('*')
+            .eq('id', userId);
+
+        if (error) {
+            return res.status(400).json({error: error.message});
+        }
+
+        if (data.length === 0) {
+            return res.status(403).json({error: 'No user found'});
+        }
+
+        const user = data[0];
+        const reqId = user.id;
+        const name = user.user_metadata?.name || null;
+        const location = user.user_metadata?.location || null;
+        const description  = user.user_metadata?.description || null;
+
+        return res.status(200).json({reqId, name, location, description});
+    } catch (err) {
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+}
+
+const setUserInfo = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const { name, location, description } = req.body;
+
+
+        const { data: existingUser, error: getError } = await supabase.auth.admin.getuserById(userId);
+
+        if (getError) {
+            return res.status(400).send({error: getError});
+        }
+
+        const updatedMetadata = {
+            ...existingUser.user_metadata,
+            name,
+            location,
+            description,
+        };
+
+        const { data, error } = await supabase.auth.admin.updateUserById(userId, {
+            user_metadata: updatedMetadata,
+        });
+
+        if (error) {
             return res.status(400).send({error: error.message});
         }
 
-        return res.status(200).send({data: data[0]});
+        return res.status(200).json({message: 'Profile Updated', user: data});
+
     } catch (err) {
         return res.status(500).send({error: 'Internal Server Error'});
     }
 }
 
-module.exports = { getReviews, createReview, getAveReview };
+module.exports = { getReviews, createReview, getAveReview, getUserInfo, setUserInfo};
