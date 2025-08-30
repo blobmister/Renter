a2e8379ec258b2007747c91148875183fdd1a4ae
Zhitian Mai <zmai0594@uni.sydney.edu.au>
2025-08-30 18:44:54 +1000

    merged the landing

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,34 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useNavigate } from 'react-router'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let navigate = useNavigate();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <div className='header'>
+        <p>Logo</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='body'>
+        <div className='sidebar'>
+          <div className='search'>
+            <p style={{margin: 0}}>Search</p>
+          </div>
+          <div className='profile'>
+            Profile
+            <button onClick={() => {console.log("hi"); navigate("/dashboard")}}>Go to Dashboard</button>
+          </div>
+        </div>
+        <div className='map'></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
