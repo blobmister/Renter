7bd80f9c7b829e8252c9edb71030bb36c07887e3
Zhitian Mai <zmai0594@uni.sydney.edu.au>
2025-08-30 16:35:08 +1000

    Add headers

--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -1,9 +1,31 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 import "./AccountForm.css"
 
-export default function Login() {
-    const login = () => {
-        console.log("post")
+export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword ] = useState("");
+
+    let navigate = useNavigate();
+
+    const login = async () => {
+        console.log(email, password)
+        const payload = {email, password}
+
+        try {
+            const response = await fetch("https://renter-production-faad.up.railway.app/api/auth/login", {
+                method: "POST",
+                body: JSON.stringify(payload)
+            });
+
+            if (response.ok) {
+                navigate("/")
+            }
+        } catch (err) {
+            console.error(err);
+        }
+
     }
 
     return (
@@ -11,10 +33,13 @@ export default function Login() {
             <div className="inputBlock">
                 <h1>RentOut</h1>
                 <p>Login to start Renting or Lending!</p>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
 
-                <button onClick={login}>Login</button>
+                <form onSubmit={login}>
+                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                
+                    <button type="submit">Login</button>
+                </form>
 
                 <p>Don't have an account? <Link to="/register">Register Now!</Link></p>
             </div>
--- front/src/Register.jsx
+++ front/src/Register.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import "./AccountForm.css"
 
 export default function Register() {
@@ -8,15 +9,32 @@ export default function Register() {
     const [name, setName ] = useState("");
     const [location, setLocation ] = useState("");
 
+    let navigate = useNavigate();
+
     const createAccount = async () => {
         console.log(email, password, name, location)
+        const payload = {name, email, password, location}
+
+        try {
+            const response = await fetch("https://renter-production-faad.up.railway.app/api/auth/register", {
+                method: "POST",
+                body: JSON.stringify(payload)
+            });
+
+            if (response.ok) {
+                navigate("/")
+            }
+        } catch (err) {
+            console.error(err);
+        }
+
     }
 
     return (
         <div className="container">
             <div className="inputBlock">
                 <h1>RentOut</h1>
-                <p>Login to start Renting or Lending!</p>
+                <p>Register an account to start Renting or Lending!</p>
 
                 <form onSubmit={createAccount}>
                     <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
