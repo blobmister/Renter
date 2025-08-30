8d63a75ac48b435c753425d45f7f81ce29882a86
JacobVeloso <jacob.a.veloso25@gmail.com>
2025-08-31 09:27:25 +1000

    Sample data for catalogue added, some issues with popup

--- front/src/Catalogue.jsx
+++ front/src/Catalogue.jsx
@@ -5,12 +5,38 @@ import TopBar from './components/topBar.jsx'
 
 // Sample data
 const items = [
-  { id: 1, name: "Product A", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "price1", image: "https://via.placeholder.com/150", lister: "lister1" },
-  { id: 2, name: "Product B", description: "desc 2", price: "price2", image: "https://via.placeholder.com/150", lister: "lister1" },
-  { id: 3, name: "Product C", description: "desc 3", price: "price3", image: "https://via.placeholder.com/150", lister: "lister1" },
-  { id: 4, name: "Product D", description: "desc 4", price: "price4", image: "https://via.placeholder.com/150", lister: "lister1" },
-  { id: 5, name: "Product E", description: "desc 5", price: "price5", image: "https://via.placeholder.com/150", lister: "lister1" },
-  { id: 6, name: "Product F", description: "desc 6", price: "price6", image: "https://via.placeholder.com/150", lister: "lister1" }
+  { id: 1, name: "Claw Hammer", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$29", image: "hammer.jpg", lister: "John Doe" },
+
+  { id: 2, name: "Zero Turn Mower", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$175", image: "mower4.png", lister: "Jessica Smith" },
+
+  { id: 3, name: "Inco ladder", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$70", image: "ladder.jpg", lister: "Stanley Johnson" },
+
+  { id: 4, name: "Electric Drill", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$20", image: "drill.jpg", lister: "Zhitian Mai" },
+
+  { id: 5, name: "fibreglass ladder", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$90", image: "ladder4.jpg", lister: "Ashley Riley" },
+
+  { id: 6, name: "Cordless Drill", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$30", image: "drilll4.jpeg", lister: "Krish Gupta" },
+
+  { id: 7, name: "Chair", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$20", image: "chair.png", lister: "Bobby Lowe" },
+
+  { id: 8, name: "Lawn Mower", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$200", image: "mower.jpeg", lister: "Benny Cook" },
+
+  { id: 9, name: "Table", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$70", image: "table.jpeg", lister: "Michael Adams" },
+
+  { id: 10, name: "Step ladder", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$75", image: "ladder2.jpg", lister: "Jacob Veloso" },
+
+  { id: 11, name: "Sofa", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$120", image: "couch.jpeg", lister: "Baily Ryan" },
+  
+  { id: 12, name: "Hammer in good condition", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$25", image: "hammer2.jpg", lister: "Marsya Amanda" },
+  
+  { id: 13, name: "brushless lawn mower", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$350", image: "mower2.jpg", lister: "Andy Jones" },
+
+  { id: 14, name: "lawn mower", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$260", image: "mower3.jpeg", lister: "Paul Jackson" },
+
+  { id: 15, name: "Framing hammer", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$30", image: "hammer3.jpg", lister: "Declan Schulz" },
+  
+  { id: 16, name: "Fibreglass step ladder", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin consequat nulla vitae tellus dictum, eget pellentesque augue sagittis. Curabitur quam risus, commodo at ultrices nec, ultrices a metus. Donec volutpat nulla id ex dignissim vestibulum. Nullam tempus pharetra odio, at rutrum risus luctus sit amet. Lorem ipsum dolor sit amet.", price: "$100", image: "ladder3.jpeg", lister: "Angela Jackson" },
+  
 ];
 
 export default function CataloguePage() {
@@ -40,8 +66,11 @@ export default function CataloguePage() {
   <>
     <div className="container">
       <TopBar align-self="flex-start"/>
-      <div className="header-section">
-        <h1>Items Available</h1>
+      {/*<div className="header-section">
+        
+      </div>
+
+      <h1>Items Available</h1>*/}
       
         {/* Search bar */}
         <input
@@ -51,13 +80,12 @@ export default function CataloguePage() {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-      </div>
 
       {/* All items */}
       <div className="grid">
         {filteredItems.map((item) => (
           <div key={item.id} className="card" onClick={() => openModal(item)}>
-            <img src={item.image} alt={item.name} />
+            <img src={`/public/assets/${item.image}`} alt={item.name} />
             <div className="card-content">
               <h2>{item.name}</h2>
               <p>{item.price || "Price not listed"}</p>  
@@ -71,7 +99,7 @@ export default function CataloguePage() {
         <div className="modal" onClick={closeModal}>
           <div className="modal-content">
             <h2>{modalItem.name}</h2>
-            <img src={modalItem.image} alt={modalItem.name} />
+            <img src={`/public/assets/${modalItem.image}`} alt={modalItem.name} />
             <h3>Description</h3>
             <p>{modalItem.description}</p>
             <h3>Lister Profile</h3>
