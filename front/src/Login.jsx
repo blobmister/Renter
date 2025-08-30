18af2d5445893eb470507bcbddd45deec50fc762
Zhitian Mai <zmai0594@uni.sydney.edu.au>
2025-08-30 15:06:06 +1000

    Basic login page design

--- front/src/Login.jsx
+++ front/src/Login.jsx
@@ -1,5 +1,20 @@
+import "./Login.css"
+
 export default function Login() {
+    const createAccount = () => {
+        console.log("post")
+    }
+
     return (
-        <p>Hello World</p>
+        <div className="container">
+            <div>
+                <input type="email" placeholder="Email" />
+            </div>
+            <div>
+                <input type="password" placeholder="Password" />
+            </div>
+
+            <button onClick={createAccount}>Login</button>
+        </div>
     );
 }
