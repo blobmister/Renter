97cbb665e05a135fb4bf97e2d1da5c479057f540
Zhitian Mai <zmai0594@uni.sydney.edu.au>
2025-08-30 16:07:28 +1000

    added states to keep track of user inputs

--- front/src/Register.jsx
+++ front/src/Register.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./AccountForm.css"
 
 export default function Register() {
-    const createAccount = () => {
-        console.log("post")
+    const [email, setEmail] = useState("");
+    const [password, setPassword ] = useState("");
+    const [name, setName ] = useState("");
+    const [location, setLocation ] = useState("");
+
+    const createAccount = async () => {
+        console.log(email, password, name, location)
     }
 
     return (
@@ -11,13 +17,15 @@ export default function Register() {
             <div className="inputBlock">
                 <h1>RentOut</h1>
                 <p>Login to start Renting or Lending!</p>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <input type="text" placeholder="Name" />
-                <input type="text" placeholder="Location" />
 
-
-                <button onClick={createAccount}>Register</button>
+                <form onSubmit={createAccount}>
+                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}/>
+                    <input type="text" placeholder="Location" value={location} onChange={(e) => setLocation(e.target.value)}/>
+                
+                    <button type="submit">Register</button>
+                </form>
 
                 <p>Already have an account? <Link to="/login">Login Now!</Link></p>
             </div>
